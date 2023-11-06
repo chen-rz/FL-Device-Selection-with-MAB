24d9fe548d1745330306ac21529269bd5e101e9e
chen-rz <chenrzjsnj@163.com>
2023-11-06 05:56:30 +0000

    20231106: Strategy to be Completed

---
+++ Coding Guide.md
@@ -0,0 +1,17 @@
+# Guide to Alternating Parameters in Code
+
+## Changing Model Nets
+### utils.py
+Network classes
+### client.py
+1. class FlowerClient self.net=
+2. def get_evaluate_fn model=
+### dataset_utils.py
+def cifar10Transformation()
+
+## torch.device
+### client.py
+1. class FlowerClient __init__
+2. def get_evaluate_fn
+
+## constants.py
---
+++ constants.py
@@ -0,0 +1,26 @@
+# Parameters
+
+# Number of dataset partions (= number of total clients)
+pool_size = 100
+
+# Number of participants in each round
+num_to_choose = 20
+
+# Global iterations
+num_rounds = 10
+
+# Channel gain
+sys_channelGain = 4e-11
+
+# Background noise
+sys_bgNoise = 1e-13
+
+# Total bandwidth
+sys_bandwidth = 1e6
+
+# Model size (bit)
+sys_modelSize = 1984192 # LeNet
+# sys_modelSize = 1825433920 # AlexNet
+
+# Global time constraint (s)
+timeConstrGlobal = 800
